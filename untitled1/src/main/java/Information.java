8d67ac572e082ff941f9cfccb1010e8af8f6afe2
Suroj2009 <xayrullayevsuroj@gmail.com>
2024-07-23 09:47:56 +0500

    mana ustoz siz uchun
    bye bye

---
+++ untitled1/src/main/java/Information.java
@@ -0,0 +1,7 @@
+public class Information {
+    public static String code1;
+    public static String code2;
+    public static String cb_price1;
+    public static String cb_price2;
+    public static double result;
+}
---
+++ untitled1/src/main/java/title2.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+
+@Data
+public class title2 {
+    private String balance;
+    private String picture;
+    private String age;
+    private String name;
+    private String gender;
+    private String company;
+    private String email;
+}
